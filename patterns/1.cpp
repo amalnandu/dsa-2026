bf646d6ec1d73aabf5fa1926187b30f01c88546e
Amal Ganesh <amal@Amals-MacBook-Air.local>
2026-06-09 23:08:39 +0530

    added pattern 1

---
+++ patterns/1.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+    int row,col;
+    cout<<"Enter rows\n";
+    cin>>row;
+    cout<<"Enter columns\n";
+    cin>>col;
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            cout<<" * ";
+        }
+        cout<<"\n";
+        
+    }
+    
+
+
+
+
+    return 0;
+}
